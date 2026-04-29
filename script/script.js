55e0494eda8ab50c354043d93b147da25a82bb15
gabrielgiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-04-29 11:40:27 -0300

    up form

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
+document.getElementById("FormLogin").addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+});
 window.location.href = "home.html";
