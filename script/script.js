b485ad1764424834e74c9afe50cec66d6a4e4e72
gabrielgiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-04-29 12:14:53 -0300

    add - função login

--- script/script.js
+++ script/script.js
@@ -1,7 +1,14 @@
 document.getElementById("FormLogin").addEventListener("submit", function (e) {
-        e.preventDefault();
-    
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    if (email === "adm@gmail.com" && senha === "123") {
+        window.location.href = "public/home.html"
+    } else {
+        document.getElementById("problema").innerHTML = "LOGIN INVÁLIDO";
+    }
+
+
 });
-window.location.href = "home.html";
